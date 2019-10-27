6652fabf0c617c75a8f36da0f078587636c8f56e
kevin princiotta <kevinprinciotta@gmail.com>
2019-10-27 17:50:16 -0400

    Candify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES += \
-    device/essential/mata/lineage_mata.mk
+    device/essential/mata/candy_mata.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mata-user \
-    lineage_mata-userdebug \
-    lineage_mata-eng
+    candy_mata-user \
+    candy_mata-userdebug \
+    candy_mata-eng
