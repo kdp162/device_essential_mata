4cd92cc2eeebbfa0af623aa5dbbee84112400f55
kevin princiotta <kevinprinciotta@gmail.com>
2019-11-08 21:07:01 -0500

    Add a powerhal

--- device.mk
+++ device.mk
@@ -293,8 +293,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.pixel-libperfmgr \
-    android.hardware.power.stats@1.0-service.pixel
+    android.hardware.power@1.2-service
 
 PRODUCT_COPY_FILES += \
     device/essential/mata/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
