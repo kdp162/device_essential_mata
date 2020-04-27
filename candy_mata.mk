25ae7e81fd20dc60e1f85e7113d0da1b34885e7d
kevin princiotta <kevinprinciotta@gmail.com>
2020-04-27 19:37:56 -0400

    Candify

--- candy_mata.mk
+++ candy_mata.mk
@@ -1,7 +1,7 @@
 # Inherit some common carbon stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/carbon/config/common.mk)
-$(call inherit-product, vendor/carbon/config/gsm.mk)
+$(call inherit-product, vendor/candy/config/common.mk)
+$(call inherit-product, vendor/candy/config/gsm.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
@@ -20,7 +20,7 @@ TARGET_RECOVERY_FSTAB := device/essential/mata/rootdir/etc/fstab.mata
 
 # Device identifiers
 PRODUCT_DEVICE := mata
-PRODUCT_NAME := carbon_mata
+PRODUCT_NAME := candy_mata
 PRODUCT_BRAND := essential
 PRODUCT_MODEL := PH-1
 PRODUCT_MANUFACTURER := Essential Products
